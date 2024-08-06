227c7fb612f020d17fe08e40ec43f8fa24538fa0
Wqkz <guillaume.farina@hotmail.com>
2024-08-06 11:12:45 +0200

    add a condition to fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,6 +16,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (!tree->left && !tree->right)
+		return (0);
+
 	height_l = binary_tree_height(tree->left);
 	height_r = binary_tree_height(tree->right);
 
